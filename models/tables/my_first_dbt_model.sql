f21a5206bf14d2d684f0935fbb1254b4d4441c2f
aaronginder <aaronleeginder@gmail.com>
2021-05-12 21:37:25 +0100

    dbt project template

---
+++ models/tables/my_first_dbt_model.sql
@@ -0,0 +1,16 @@
+-- {{ config(materialized='table') }}
+WITH source_data AS (
+    SELECT
+        1 AS id
+    UNION
+    ALL
+    SELECT
+        NULL AS id
+)
+
+SELECT
+    *
+FROM
+    source_data
+WHERE
+    id IS NOT NULL
